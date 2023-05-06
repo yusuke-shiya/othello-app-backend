708d41c96ad5d966f2462847a7b6911c22396597
志谷優介 <yusuke.shiya@gmail.com>
2023-05-06 16:42:59 +0900

    ルームごとの入退室情報を管理し、対戦状況をブロードキャストする

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :unique_id
+
+    # グローバルなバトルルームの状態を保持するハッシュ
+    @@room_states = {}
+
+    def connect
+      self.unique_id = SecureRandom.uuid
+    end
+
+    def room_states
+      @@room_states
+    end
   end
 end
--- app/channels/battle_room_channel.rb
+++ app/channels/battle_room_channel.rb
@@ -1,14 +1,47 @@
 class BattleRoomChannel < ApplicationCable::Channel
   def subscribed
     @room_id = params[:room_id]
+    # ルームIDと手番を記録
+    @player = {
+      id: unique_id,
+      color: room_states[@room_id]&.first&.[](:color) == 'black' ? 'white' : 'black'
+    }
+    return reject unless can_join_room?
+
     stream_from "battle_room_channel_#{@room_id}"
+    update_room_state
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    # クライアントが切断したらルームから削除
+    room_states[@room_id]&.delete(@player)
   end
 
   def receive(data)
     ActionCable.server.broadcast("battle_room_channel_#{@room_id}", data)
+  end
+
+  private
+
+  def room_states
+    connection.room_states
+  end
+
+  def can_join_room?
+    players = room_states[@room_id] || []
+    players.count < 2
+  end
+
+  def update_room_state
+    players = room_states[@room_id] || []
+    players << @player
+    logger.debug(players)
+    room_states[@room_id] = players
+
+    if players.count == 1
+      ActionCable.server.broadcast("battle_room_channel_#{@room_id}", { battleStatus: 'waiting' })
+    elsif players.count == 2
+      ActionCable.server.broadcast("battle_room_channel_#{@room_id}", { battleStatus: 'playing' })
+    end
   end
 end
