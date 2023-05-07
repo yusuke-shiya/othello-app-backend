36d126a8b0168fb5807063fa7a5b6b0b6c362010
志谷優介 <yusuke.shiya@gmail.com>
2023-05-07 20:43:26 +0900

    どちらかのプレイヤーの接続が切れても再度接続したクライアントを空いている手番としてゲームが継続できるようにする

--- app/channels/battle_room_channel.rb
+++ app/channels/battle_room_channel.rb
@@ -1,48 +1,59 @@
 class BattleRoomChannel < ApplicationCable::Channel
   def subscribed
-    @room_id = params[:room_id]
-    # ルームIDと手番を記録
-    @player = {
-      id: unique_id,
-      color: room_states[@room_id]&.first&.[](:color) == 'black' ? 'white' : 'black'
-    }
     return reject unless can_join_room?
 
-    stream_from "battle_room_channel_#{@room_id}"
+    stream_from "battle_room_channel_#{room_id}"
 
     # 接続したプレイヤーがどちらの手番かを送信
-    transmit({ myColor: @player[:color] })
+    transmit({ myColor: player[:color] })
     # ルームの状態を更新
     update_room_state
-    battle_status = room_states[@room_id].count == 2 ? 'playing' : 'waiting'
-    ActionCable.server.broadcast("battle_room_channel_#{@room_id}",
-                                 { battleStatus: battle_status })
+    ActionCable.server.broadcast("battle_room_channel_#{room_id}", room_states[room_id])
   end
 
   def unsubscribed
     # クライアントが切断したらルームから削除
-    room_states[@room_id]&.delete(@player)
+    room_states[room_id][:players].delete(player)
   end
 
   def receive(data)
-    ActionCable.server.broadcast("battle_room_channel_#{@room_id}", data)
+    room_states[room_id][:othello] = data
+    ActionCable.server.broadcast("battle_room_channel_#{room_id}", room_states[room_id])
   end
 
   private
+
+  def room_id
+    @room_id ||= params[:room_id]
+  end
+
+  def player
+    @player ||= {
+      id: unique_id,
+      color: current_players.first&.dig(:color) == 'black' ? 'white' : 'black'
+    }
+  end
 
   def room_states
     connection.room_states
   end
 
+  def current_players
+    room_states[room_id]&.dig(:players) || []
+  end
+
   def can_join_room?
-    players = room_states[@room_id] || []
-    players.count < 2
+    current_players.count < 2
   end
 
   def update_room_state
-    players = room_states[@room_id] || []
-    players << @player
-    logger.debug(players)
-    room_states[@room_id] = players
+    players = current_players
+    players << player
+
+    room_states[room_id] = {
+      players: players,
+      battleStatus: players.count == 2 ? 'playing' : 'waiting',
+      othello: room_states[room_id]&.dig(:othello)
+    }
   end
 end
